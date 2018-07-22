e39d8f350159b18222785627607299c844f815f1
monica <346429553@qq.com>
2018-07-22 19:26:51 +0800

    这是monica的第一次提交

---
+++ c4.py
@@ -0,0 +1,16 @@
+print("a")
+print('a')
+print('"a"')
+print("'a'")
+print('''ab"c'dddd''')
+print("""ab"c'dddd""")
+
+
+
+c = '''
+ab"c'dddd
+
+hdhhd
+'''
+
+# print(c)
---
+++ hello.py
@@ -0,0 +1,62 @@
+# print("hello world!")
+
+
+# def s(a, b):
+#     return a + b
+
+# name = input('姓名:')
+
+# if name == "郑俊玲":
+#     print("你好" + name)
+# else:
+#     print(name + "错误")
+
+
+
+# for i in range(0, 2020):
+#     if i%4==0:
+#         if i%100==0:
+#             if i%400==0:
+#                 print(i)
+#         else:
+#             print(i)
+
+l = [1, 2, 3, 4, 5]
+k = (1,2,3,4,5)
+
+print(l[1])
+# b = ['abc', 'dddd', '你好']
+
+# c = [l, b]
+
+# print(c)
+
+# d = {'name': 'a', 'age': 20, 'c': c}
+
+
+# print(d)
+
+
+# y = [d]
+# print(y)
+
+# print(l[2:])
+
+# print(d['c'][0][3])
+# print(d)
+
+# print("-----")
+
+# for k, v in d.items():
+#     print('{}: {}'.format(k, v))
+# a = ['张三','李四','王五']
+# b = [10,12,18]
+# c = [30,40,80]
+# class1 = {'name':a,'age':b,'wet':c}
+# print(class1)
+p1 ={'name':'zhangsan','age':10,'wet':30}
+p2 ={'name':'zhangsan1','age':11,'wet':31}
+p3 ={'name':'zhangsan2','age':12,'wet':32}
+a =[p1,p2,p3]
+for p in a:
+    print(p['name'])
