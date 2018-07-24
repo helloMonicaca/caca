5969ff50a0fca4dc7ac6daed9e0943ae77143b95
monica <346429553@qq.com>
2018-07-24 15:46:14 +0800

    add function

--- c4.py
+++ c4.py
@@ -7,8 +7,15 @@ print("""ab"c'dddd""")
 
 
 
-c = '''
-ab"c'dddd
+# c = '''
+# ab"c'dddd
 
-hdhhd
-'''
+# hdhhd
+# '''
+
+# print("hallo word!")
+# print"hallo again"
+# print"i like typing this"
+# print"this is fun"
+# print"I'd much rather you 'not'."
+# print'I "said" do not touch this.'
---
+++ function.py
@@ -0,0 +1,40 @@
+# print("hallo word!")
+# print("hallo again")
+# print("i like typing this")
+# print("this is fun")
+# print("I'd much rather you 'not'.")
+# print('I "said" do not touch this.')
+# print('''
+# I "said" 
+# do not touch this.
+# ''')
+# print(80-2)
+# print(40-5*2/10)
+# print("what is 7 plus 8 ?")
+# print(7+8)
+# print(7%2)
+# my_name = 'Zed A. Shaw'
+# my_age = 35
+# my_height = 70
+# my_weight = 180
+# my_eyes = 'blue'
+# my_teeth = 'white'
+# my_hair = 'brown'
+# print("let's talk about %s." % my_name) 
+# print("H's %d inches tall." % my_height)
+# print("H's %d pounds heavy." % my_weight)
+# print("H's got %s hair and %s eyes." % (my_hair,my_hair))
+# print("H's teeth are usually %s depending on the coffee." % my_teeth)
+# print("If i add %d,%d,and %d I get %d." % (my_age,my_height,my_weight,my_age + my_height + my_weight))
+# def message(name, age):
+#     print("姓名: %s,年龄: %d" %(name,age))
+
+# message("xiaoma",20)
+# def plan(shengfen, city, youbian):
+#     text = "我叫王大锤，我来自 %s，%s，我家邮政编码是 %d" % (shengfen, city, youbian)
+#     return text
+
+# t = plan("山东", "济南", 10010)
+# print(t)
+# plan("北京","海淀", 10086)
+
